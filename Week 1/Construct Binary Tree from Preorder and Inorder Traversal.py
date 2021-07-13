ce3961fbfec16224206dae53347463455d9fde75
beshirD <beshirdek@gmail.com>
2021-07-13 10:44:23 +0300

    Week 1 Trees

---
+++ Week 1/Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        index = [0]
+        inorder_dict = {}
+        for i in range(len(inorder)):
+            inorder_dict[inorder[i]] = i
+        return self.construct_BT(preorder,inorder_dict,0,len(inorder) - 1, index)
+            
+    def construct_BT(self,preorder,inorder_dict,start, end,index):
+        if start > end:
+            return None
+        root = TreeNode(preorder[index[0]])
+        idx = inorder_dict[preorder[index[0]]]
+        index[0] += 1
+        root.left = self.construct_BT(preorder,inorder_dict,start,idx - 1,index)
+        root.right = self.construct_BT(preorder,inorder_dict,idx + 1,end,index)
+        
+        return root
---
+++ Week 1/House Robber III.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        robbed_dict = {}
+        not_robbed_dict = {}
+       
+        return self.rob_house(root,robbed_dict,not_robbed_dict,False)
+    
+    def rob_house(self,node,robbed_dict,not_robbed_dict,parent_robbed):
+        if not node:
+            return 0
+        
+        if parent_robbed:
+            if node in not_robbed_dict:
+                return not_rob[node]
+            
+            not_robbed_dict[node] = self.rob_house(node.left,robbed_dict,not_robbed_dict,False) +                                                       self.rob_house(node.right,robbed_dict,not_robbed_dict,False)
+            
+            return not_robbed_dict[node]
+        else:
+            
+            if node in robbed_dict:
+                return robbed_dict[node]
+            
+            rob = node.val + self.rob_house(node.left,robbed_dict,not_robbed_dict,True) +                                             self.rob_house(node.right,robbed_dict,not_robbed_dict,True)
+            
+            not_rob = self.rob_house(node.left,robbed_dict,not_robbed_dict,False) +                                                       self.rob_house(node.right,robbed_dict,not_robbed_dict,False)
+            
+            robbed_dict[node] = max(rob,not_rob)
+            
+            return robbed_dict[node]
